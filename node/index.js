933a7952e03b7e12a3c783fc5f72c7de55dc5640
David Santos <davidm.silva2@gmail.com>
2023-09-08 18:15:29 -0300

    Alteracoes no compose e docker file node

--- node/index.js
+++ node/index.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-// const config = {
-//   host: "db",
-//   user: "root",
-//   password: "root",
-//   database: "nodedb",
-// };
-// const mysql = require("mysql");
-// const connection = mysql.createConnection(config);
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+const mysql = require("mysql");
+const connection = mysql.createConnection(config);
 
-// const sql = `INSERT INTO people(name) values('David')`;
-// connection.query(sql);
-// connection.end();
+const sql = `INSERT INTO people(name) values('David')`;
+connection.query(sql);
 
 app.get("/", (req, res) => {
-  res.send("<h1>Full Cycle</h1>");
+  connection.query("SELECT name FROM people", (error, results, fields) => {
+    if (error) {
+      console.error("Erro:", error);
+      res.status(500).send("Erro ao buscar dados do banco de dados.");
+      return;
+    }
+    res.send(`<h1>Full Cycle Rocks!</h1>
+      <ol>
+        ${
+          !!results.length
+            ? results.map((r) => `<li>${r.name}</li>`).join("")
+            : ""
+        }
+      </ol>
+    `);
+  });
 });
 
 app.listen(port, () => {
